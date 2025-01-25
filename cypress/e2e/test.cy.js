ad52c5f064e1edad4795e2c8effcfbe2c2b8bdac
Laura Majey <lauramajey@gmail.com>
2025-01-24 19:16:09 -0500

    Descripción

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -2,28 +2,33 @@ describe('prueba de busqueda de carros', () => {
   
   it('abri pagina', () => {
     cy.viewport(1024, 768)
+
     //Ingresa al sitio 
     cy.visit('https://www.viajemos.com/es/')
-    //Ingresa la localidad de reecogidad
+
+    //Ingresa la localidad de recogida
     cy.get('[data-link="pickupLocation"]').type('Barcelona Airport');
     cy.get('#ui-id-1 li', { timeout: 10000 }).should('exist');
     cy.get('#ui-id-1 li')
     .contains('Aeropuerto de Barcelona, Barcelona, España') 
     .click();
-    //Habilita la opción de recogida en otra localidad
+    
+    //Habilita la opción de entrega en otra localidad
     cy.get('.switch__slider.switch__slider--round').first()
       .click();
 
-    //Ingresa la localidad de reecogidad
+    //Ingresa la localidad de entrega
     cy.get('[data-link="returnLocation"]').type('Madrid');
     cy.get('#ui-id-2 li', { timeout: 10000 }).should('exist');
     cy.get('#ui-id-2 li')
     .contains('Aeropuerto de Madrid Barajas, Madrid, España') 
     .click();
-    //Ingresa al modal de calendario, dando click a el pick up date 
+
+    //Ingresa al modal de calendario, dando clic a el pick up date 
     cy.get('[data-link="pickupDateCarModal"]') 
     .should('be.visible')  
     .click();
+
     //Verifica que el modal del calendario exista 
     cy.get('.container__months').should('exist');
 
@@ -35,6 +40,7 @@ describe('prueba de busqueda de carros', () => {
     }
     //Verifica que el mes de julio este visible 
     cy.get('.month-item-name').eq(1).should('contain', 'julio')
+
     //Selecciona el día 20
     cy.get('.month-item').eq(1) 
     .find('.day-item') 
@@ -46,11 +52,14 @@ describe('prueba de busqueda de carros', () => {
     .find('.day-item') 
     .contains('22') 
     .click();
-    //Clic en el botón 'Buscar'
+
+    //Clic en el botón buscar
     cy.get('.quicksearch__button.button-default--qs').first()
     .click();
-    //Verifica que las tarjetas de vehiculo esten activas (disponibilidad)
+
+    //Verifica que las tarjetas de vehículo esten activas (disponibilidad)
     cy.get('.card-vehicle').should('be.visible');
+
     //Verifica en las tarjetas del vehiculo que la informacion este visible
     cy.get('.card-vehicle').each(($vehicle) => {
 
